8c1c729d109f9675fddce6ff36030f6cf409a8d0
Najem <najem.334@hotmail.com>
2023-01-25 16:40:56 +0300

    car in controllers

--- rentcar-be/routes/cars.js
+++ rentcar-be/routes/cars.js
@@ -1,1 +1,16 @@
-a
+const express = require('express');
+const router =  express.Router();
+
+router.use(express.json());
+
+//const isLoggedIn = require("../helper/isLoggedIn");
+
+const carController = require('../controllers/cars');
+
+router.post('/car/add', carsController.add_post);
+router.get('/car/index', carsController.index_get); //read list
+router.get('/car/edit', carsController.edit_get); //read item to edit it
+router.put('/car/update', carsController.update_put);
+router.delete('/car/delete', carsController.delete_delete); 
+
+module.exports = router;
