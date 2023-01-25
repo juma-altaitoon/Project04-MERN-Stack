373086791c5ea42a145dd30692601035f0cabe78
yousef <youseeff012@gmail.com>
2023-01-25 17:23:42 +0300

    Model Done for User

--- rentcar-be/models/User.js
+++ rentcar-be/models/User.js
@@ -1,1 +1,24 @@
-a
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    birthdate: Date,
+    gender:Boolean,
+    nationality:String,
+    national_id:String,
+    phone_number:String,
+    email_address:String,
+    password:String,
+    documents:String,
+    license_issued:Date,
+    license_expiry:Date,
+    user_type:Boolean,
+    comment:String
+
+
+
+},{timestamps: true}) 
+
+const User = mongoose.model("User", userSchema , "User");
+module.exports = User;
